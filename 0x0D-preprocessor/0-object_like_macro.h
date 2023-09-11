504685e5e4a47b4281cb65288cf38867743dffc4
rockefellere <rockefellere@gmail.com>
2023-09-11 08:16:41 -0700

    a macro defination that replaces SIZE with 1024

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,10 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 0-object_like_macro
+ *
+ * Desc: header file
+ */
 #define SIZE 1024
 
 #endif
-
